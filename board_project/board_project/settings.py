b6483456f869f8c7f40dd7ad53147cab9030d116
LAB13_USER ON WS35 <L13X16W35@LabHCI.net>
2025-09-24 17:48:59 +0800

    feature(board): setup templates

--- board_project/board_project/settings.py
+++ board_project/board_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'board_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates/board_app"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
